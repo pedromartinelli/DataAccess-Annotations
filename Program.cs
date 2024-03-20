a1b96ec2af2ea086be62bd8caa400e50646c2139
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-20 13:50:32 -0300

    acesso a dados com ADO.NET

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string de conexão com o banco
+            const string connectionString = "Server=localhost,1433;Database=balta;User Id=sa;Password=1q2w3e!@#; TrustServerCertificate=true;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Conectado!");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+
+
+            // Microsoft.Data.SqlClient
+        }
+    }
+}
