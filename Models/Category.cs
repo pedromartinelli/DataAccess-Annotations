c11846fb48ad0dfd13c72ec7f6d3810755c136ce
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-20 14:33:15 -0300

    primeira consulta com dapper

---
+++ Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = null!;
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using System.Data;
+﻿using Dapper;
+using DataAccess.Models;
+using Microsoft.Data.SqlClient;
 
 namespace DataAccess
 {
@@ -10,25 +11,13 @@ namespace DataAccess
             // string de conexão com o banco
             const string connectionString = "Server=localhost,1433;Database=balta;User Id=sa;Password=1q2w3e!@#; TrustServerCertificate=true;";
 
-            using (var connection = new SqlConnection(connectionString))
+            using var connection = new SqlConnection(connectionString);
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+
+            foreach (var category in categories)
             {
-                Console.WriteLine("Conectado!");
-                connection.Open();
-
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
-                }
+                Console.WriteLine($"{category.Id} - {category.Title}");
             }
-
 
             // Microsoft.Data.SqlClient
         }
